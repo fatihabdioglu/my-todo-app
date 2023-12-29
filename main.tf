adbc1d5b7083b2cb698033bc9ede9f095b9dd0d4
fatihabdioglu <f.abdioglu52@gmail.com>
2023-12-29 22:21:26 +0000

    app added

--- main.tf
+++ main.tf
@@ -25,10 +25,10 @@ variable "user" {
 }
 
 resource "aws_instance" "managed_nodes" {
-  ami = "ami-05a5f6298acdb05b6"
+  ami = "ami-023c11a32b0207432"
   count = 3
   instance_type = "t2.micro"
-  key_name = "mackeypair"  # change with your pem file
+  key_name = "firstkey"  # change with your pem file
   vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
   iam_instance_profile = "jenkins-project-profile-${var.user}" # we created this with jenkins server
   tags = {
