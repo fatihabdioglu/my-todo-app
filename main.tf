6bc1669fd8c95c0ada9ac0e9bbaaee73eba7f46c
fatihabdioglu <f.abdioglu52@gmail.com>
2023-12-29 22:11:00 +0000

    app added

--- main.tf
+++ main.tf
@@ -4,13 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-  }
-  backend "s3" {
-    bucket = "fatih-project-todo-app"
-    key = "backend/tf-backend-jenkins.tfstate"
-    region = "us-east-1"
-    
-  }
 }
 
 provider "aws" {
